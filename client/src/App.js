ef815d06bba3fae23ceffd443cbf89584966c200
MarcinBalejko <mbbm9@o2.pl>
2020-08-02 01:39:54 +0200

    :white_check_mark::recycle: Major clean-up, added axios, added proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
 
-const App = () => {
-  <div className="App"></div>;
-};
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" /> React File Uploader
+    </h4>
+  </div>
+);
 
 export default App;
