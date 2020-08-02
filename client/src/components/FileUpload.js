cf9f061e6d06f5939bf11022c0e7f4bb10fc5a36
MarcinBalejko <mbbm9@o2.pl>
2020-08-02 18:23:50 +0200

    :white_check_mark: Added FileUpload component

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
-export const FileUpload = () => {
-  return <div></div>;
+const FileUpload = () => {
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("Choose File");
+
+  const onChange = (e) => {};
+
+  return (
+    <Fragment>
+      <form>
+        <div className="custom-file mb-4">
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+          />
+          <label className="custom-file-label" htmlFor="customFile">
+            {filename}
+          </label>
+        </div>
+
+        <input
+          type="submit"
+          value="Upload"
+          className="btn btn-primary btn-block mt-4"
+        />
+      </form>
+    </Fragment>
+  );
 };
+
+export default FileUpload;
